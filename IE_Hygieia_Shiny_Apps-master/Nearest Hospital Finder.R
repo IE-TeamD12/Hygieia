f08b9f35bea1429748a324ca0269c512c50b5ef1
Shiivong Birla <shiivongbirla27@gmail.com>
2020-06-02 13:12:28 +1000

    Update Nearest Hospital Finder.R

--- IE_Hygieia_Shiny_Apps-master/Nearest Hospital Finder.R
+++ IE_Hygieia_Shiny_Apps-master/Nearest Hospital Finder.R
@@ -9,30 +9,42 @@ library(shiny)
 library(leaflet)
 library(shinyalert)
 library(shinydashboard)
+library(dashboardthemes)
 library(sp)
 library(leaflet)
+library(ggmap)
 library(readxl)
 
 Hospitals <- read_excel("Hospital_Locations.xlsx") # Reading in the Hospitals Data File
 
-langVector <- c("English", "Kannada", "Hindi", "Malayalam", "Bengali", "Persian", "Bosnian", "Croatian", "Serbian", "Cantonese",
-                "Chinese", "Punjabi", "Urdu", "Greek", "Mandarin", "French", "Turkish", "Farsi", "Tamil", "Italian", "Macedonian",
-                "Spanish", "Marathi", "Sinhalese", "Arabic", "Russian", "Vietnamese", "Polish", "Romanian", "Malay", "Gujarati",
-                "Czech", "Slovak", "Telugu", "Zulu", "Ukranian")
+langVector <- c("ENGLISH", "ITALIAN", "MANDARIN", "HINDI")
 
-langProb <- c(0.65, rep(0.1, 35)) # Creating the language probabilities, English being the highest
+langProb <- c(rep(0.25, 4)) # Creating the language probabilities, English being the highest
 
 set.seed(30046572) # Setting seed, using my student id for it
 
 Hospitals$Languages <- sample(langVector, size = nrow(Hospitals), replace = TRUE, prob = langProb) # Randomly sampling the languages, also ading a new column to the dataset
 
-Hospitals$Languages[Hospitals$LGAName == "MELBOURNE" & Hospitals$Type == "PRIVATE"] <- "English" # Changing the Language to English to show clustering effect at launching
+Hospitals$Languages[Hospitals$LGAName == "MELBOURNE" & Hospitals$Type == "PRIVATE"] <- "ENGLISH" # Changing the Language to English to show clustering effect at launching
+
+logo_turquoise_gradient <- shinyDashboardLogoDIY( # Making a customised header logo which will appear on the top left part of the application
+  
+  boldText = ""
+  ,mainText = ""
+  ,textSize = 30
+  ,badgeText = "HOSPITAL FINDER" # Header text
+  ,badgeTextColor = "white"
+  ,badgeTextSize = 3.5
+  ,badgeBackColor = "#40E0D0"
+  ,badgeBorderRadius = 10
+  
+)
 
 # UI side processing design
 
-ui <- dashboardPage(skin = "blue", # Giving a blue header
+ui <- dashboardPage(
                     
-                    dashboardHeader(title = "Hospital Finder" # Dashboard title
+                    dashboardHeader(title = logo_turquoise_gradient # Dashboard title
                                     
                     ),
                     
@@ -62,12 +74,12 @@ ui <- dashboardPage(skin = "blue", # Giving a blue header
                                                           
                                                           choices = c(unique(as.character(Hospitals$Languages))), # Giving all the options for Hospital Type
                                                           
-                                                          selected = "English", multiple = FALSE)) # Declaring multi-select as FALSE
+                                                          selected = "ENGLISH", multiple = FALSE)) # Declaring multi-select as FALSE
                     ))
                       
                     ), # Initializing a sidebar
                     
-                    dashboardBody(useShinyalert(), # Shiny alert for cool popups
+                    dashboardBody(useShinyalert(), shinyDashboardThemes(theme = "grey_dark"), # Shiny alert for cool popups
                                   
                                   tags$head(tags$style(HTML('                       
                                                             .main-header .logo {
@@ -79,13 +91,50 @@ ui <- dashboardPage(skin = "blue", # Giving a blue header
                                                             }
                                                             '))), # A CSS template for the font size and font
                                   
+                                  tags$style(type="text/css", # Making a custom CSS class to hide all errors popping up on the front end
+                                             ".shiny-output-error { visibility: hidden; }",
+                                             ".shiny-output-error:before { visibility: hidden; }"
+                                  ),
+                                  
+                                  tags$style(HTML(" 
+                                             .dataTables_wrapper .dataTables_length, .dataTables_wrapper .dataTables_filter, .dataTables_wrapper .dataTables_info, .dataTables_wrapper .dataTables_processing, .dataTables_wrapper .dataTables_paginate, .dataTables_wrapper .dataTables_paginate .paginate_button.current:hover {
+                                             color: #ffffff;
+                                             }
+                                             .dataTables_wrapper .dataTables_paginate .paginate_button{box-sizing:border-box;display:inline-block;min-width:1.5em;padding:0.5em 1em;margin-left:2px;text-align:center;text-decoration:none !important;cursor:pointer;*cursor:hand;color:#ffffff !important;border:1px solid transparent;border-radius:2px}
+
+
+                                             .dataTables_length select {
+                                             color: #ffffff;
+                                             background-color: #0E334A
+                                             }
+
+
+                                             .dataTables_filter input {
+                                             color: #0E334A;
+                                             background-color: #ffffff
+                                             }
+
+                                             thead {
+                                             color: #ffffff;
+                                             }
+
+                                             tbody {
+                                             color: #ffffff;
+                                             }" # Custom CSS code to change the color of the 
+                                                  
+                                                  
+                                  )),
+                    
+                                  
+                                  
+                                  
                                   fluidRow(
                                     
-                                    tabBox(height = "1200px", width = "1000px",
+                                    tabBox(height = "1220px", width = "1000px",
                                            
                                            tabPanel(title = tagList(icon("project-diagram", class = "fas fa-project-diagram") # Creating the tabset panels
                                                                     
-                                                                    , "SPREAD OF HOSPITALS ACROSS VICTORIA"),
+                                                                    , "HOSPITALS ACROSS VICTORIA"),
                                                     
                                                     box(leafletOutput("HospitalMap"), status = "primary", solidHeader = TRUE, # Hospital distribution map of Vitoria
                                                         
@@ -104,8 +153,10 @@ ui <- dashboardPage(skin = "blue", # Giving a blue header
 
 server <- function(input, output, session) { # Starting the server side application
   
-  shinyalert(title = "Nearest Hospital Finder!", type = "info", text = "Select the Suburb in which you want to find Hospitals, Type of Hospital and Preferred Language you want
-             using the dropdown menu on the left and we will find you the best Hospitals!") # Shinyalert to let the user know what the widget is about
+  shinyalert(title = "WELCOME TO OUR NEAREST HOSPITAL FINDER APP!", type = "info", text = paste("Select the desired", paste(strong("local government area, type of hospital and language preference"), "on the left
+             and we will find you the best hospitals!")), confirmButtonText = "GOT IT!",
+             confirmButtonCol = "#40E0D0", html = TRUE,
+             closeOnClickOutside = TRUE) # Shinyalert to let the user know what the widget is about
   
   
   output$HospitalMap <- renderLeaflet({ # Declaring the leaflet processing function using shiny's renderLeaflet
@@ -116,12 +167,13 @@ server <- function(input, output, session) { # Starting the server side applicat
     
         Language_Filtered <- input$LANGUAGE
     
-        if(LGA_Filtered != "All" & Type_Filtered != "All" & Language_Filtered != "All") { # Establishing an if-else condition based on user input, to restrict the data if "All" is not selected in the filter
+        ifelse(LGA_Filtered != "All" & Type_Filtered != "All" & Language_Filtered != "All",
+               Hospitals_Filtered <- subset(Hospitals, LGAName == LGA_Filtered & Type == Type_Filtered & Languages == Language_Filtered))  # Establishing an if-else condition based on user input, to restrict the data if "All" is not selected in the filter
       
-          Hospitals_Filtered <- subset(Hospitals, LGAName == LGA_Filtered & Type == Type_Filtered & Languages == Language_Filtered) # Filtering Hospital data based on some condition
+           # Filtering Hospital data based on some condition
       
-          }
-    
+        
+        
         Hospitals_Filtered <- as.data.frame(Hospitals_Filtered) # Converting a matrix into a dataframe
     
     
@@ -129,7 +181,14 @@ server <- function(input, output, session) { # Starting the server side applicat
     
     
         leaflet(Hospitals_Filtered) %>% addTiles() %>% # Calling the leaflet function on our filtered data and calling addTiles on top of it
-                                    addMarkers(clusterOptions = markerClusterOptions(), # Adding markers on the map and clustering them
+                                    addMarkers(clusterOptions = markerClusterOptions(iconCreateFunction =
+                                                                                       JS("
+                                          function(cluster) {
+                                             return new L.DivIcon({
+                                               html: '<div style=\"background-color:rgba(77,77,77,1.5)\"><span>' + cluster.getChildCount() + '</div><span>',
+                                               className: 'marker-cluster'
+                                             });
+                                           }")), # Adding markers on the map and clustering them
                                     label = toupper(Hospitals_Filtered$Hospital)) # Giving the markers a label and an icon
                                     
   })
@@ -148,7 +207,8 @@ server <- function(input, output, session) { # Starting the server side applicat
       
           if(nrow(Hospitals_Filtered) == 0) { # Checking the condition of exhausting data
         
-          shinyalert(title = "Not enough data!", type = "error", text = "Sorry, there are no Hospitals with the selected filters!") # Displaying an error for running out of data
+          shinyalert(title = "NO HOSPITALS FOUND!", type = "error", text = paste("Please change the", strong("selected filters!")),
+                     closeOnClickOutside = TRUE, html = TRUE) # Displaying an error for running out of data
         
           }
       
